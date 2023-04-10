1b2be0633b625c804a677e774a0e2a0665c7a7dd
Jamal <x5196552@gmail.com>
2023-04-10 10:33:18 +0300

    feat: order memory done

--- src/resources/orders/order.memory.repository.js
+++ src/resources/orders/order.memory.repository.js
@@ -1,7 +1,44 @@
 import Order from './order.model';
 
-const getAll = async () =>
-  // TODO: mock implementation. should be replaced during task development
-  [];
+const Orders = [
+  new Order({ clientId: '1', productList: [], isDelivered = false}),
+  new Order({ clientId: '2', productList: [], isDelivered = false}),
+];
 
-export { getAll };
+const getAll = async () => Orders;
+
+const getOrderById = async (id) => Orders.find((order) => id === order.id);
+
+const createOrder = async ({ id, clientId, productList, isDelivered }) => {
+  const order = new Order({ id, clientId, productList, isDelivered });
+  Orders.push(order);
+  return order;
+};
+
+const deleteById = async (id) => {
+  const orderPosition = Orders.findIndex((order) => id === order.id);
+
+  if (orderPosition === -1) return null;
+
+  Orders.splice(orderPosition, 1);
+};
+
+const updateById = async ({ id, clientId, productList, isDelivered }) => {
+  const orderPosition = Orders.findIndex((order) => id === order.id);
+
+  if (orderPosition === -1) return null;
+
+  const oldOrder = Orders[orderPosition];
+  const newOrder = { ...oldOrder, clientId, productList, isDelivered };
+
+  Orders.splice(orderPosition, 1, newOrder);
+};
+
+export {
+  Orders,
+  getAll,
+  getOrderById,
+  createOrder,
+  deleteById,
+  updateById,
+};
