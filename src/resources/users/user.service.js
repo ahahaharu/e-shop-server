3612b91ffa907758e81b9cb783baad63752b7988
Pereverzev Marat <marat.pereverzrv@gmail.com>
2023-04-10 18:53:18 +0300

    feat: user service done

--- src/resources/users/user.service.js
+++ src/resources/users/user.service.js
@@ -1,5 +1,23 @@
 import * as usersRepo from './user.memory.repository.js';
-
+import * as ordersRepo from '../orders/order.memory.repository.js';
 const getAll = () => usersRepo.getAll();
-
-export { getAll };
+const getById = (id) => usersRepo.getUserById(id);
+const createUser = ({ id, firstName, lastName, login, password, phoneNumber }) => {
+  usersRepo.createUser({ id, firstName, lastName, login, password, phoneNumber });
+}
+const deleteById = async (id) => {
+  for(let order of ordersRepo.Orders){
+    ordersRepo.deleteById(order.id, id);
+  }
+  usersRepo.deleteById(id);
+}
+const updateById = ({ id, firstName, lastName, login, password, phoneNumber }) => {
+  usersRepo.updateById({ id, firstName, lastName, login, phoneNumber });
+}
+export { 
+  getAll,
+  getById,
+  createUser,
+  deleteById,
+  updateById
+};
