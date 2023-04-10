f7ca84cb50e1eb893721400b76c05be5fd003914
Pereverzev Marat <marat.pereverzrv@gmail.com>
2023-04-10 22:28:17 +0300

    refactor: fixed order service delete method

--- src/resources/orders/order.service.js
+++ src/resources/orders/order.service.js
@@ -6,8 +6,8 @@ const getById = (id) => ordersRepo.getOrderById(id);
 const createOrder = ({ id, clientId, productList, isDelivered }) => {
   ordersRepo.createOrder({ id, clientId, productList, isDelivered });
 }
-const deleteById = async (userId, orderId) => {
-  ordersRepo.deleteById(userId, orderId);
+const deleteById = async (orderId) => {
+  ordersRepo.deleteById(orderId);
 }
 const updateById = ({ id, clientId, productList, isDelivered }) => {
   ordersRepo.updateById({ id, clientId, productList, isDelivered });
