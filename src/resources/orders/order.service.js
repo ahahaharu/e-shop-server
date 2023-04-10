20f458b655a7969e6964e15e84e119267f15e0a3
Pereverzev Marat <marat.pereverzrv@gmail.com>
2023-04-10 18:52:57 +0300

    feat: order service done

--- src/resources/orders/order.service.js
+++ src/resources/orders/order.service.js
@@ -1,5 +1,21 @@
 import * as ordersRepo from './order.memory.repository.js';
+import * as usersRepo from '../users/user.memory.repository.js';
 
 const getAll = () => ordersRepo.getAll();
-
-export { getAll };
+const getById = (id) => ordersRepo.getOrderById(id);
+const createOrder = ({ id, clientId, productList, isDelivered }) => {
+  ordersRepo.createOrder({ id, clientId, productList, isDelivered });
+}
+const deleteById = async (userId, orderId) => {
+  ordersRepo.deleteById(userId, orderId);
+}
+const updateById = ({ id, clientId, productList, isDelivered }) => {
+  ordersRepo.updateById({ id, clientId, productList, isDelivered });
+}
+export { 
+  getAll,
+  getById,
+  createOrder,
+  deleteById,
+  updateById
+};
