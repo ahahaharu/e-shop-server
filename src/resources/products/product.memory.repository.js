c51be4474fe3a64586eca669d5762efc29183462
Jamal <x5196552@gmail.com>
2023-04-10 10:10:26 +0300

    feat: product memory done

--- src/resources/products/product.memory.repository.js
+++ src/resources/products/product.memory.repository.js
@@ -1,7 +1,44 @@
 import Product from './product.model';
 
-const getAll = async () =>
-  // TODO: mock implementation. should be replaced during task development
-  [];
+const Products = [
+  new Product({title: 'Nigger', price: 1, description: 'black', count: 5}),
+  new Product({title: 'Aegis BOOST LE', price: 100, description: 'white', count: 1}),
+];
 
-export { getAll };
+const getAll = async () => Products;
+
+const getProductById = async (id) => Products.find((product) => id === product.id);
+
+const createProduct = async ({id, title, price, description, count}) => {
+  const product = new Product({id, title, price, description, count});
+  Products.push(product);
+  return product;
+};
+
+const deleteById = async (id) => {
+  const productPosition = Products.findIndex((product) => id === product.id);
+
+  if (productPosition === -1) return null;
+
+  Products.splice(productPosition, 1);
+};
+
+const updateById = async ({id, title, price, description, count}) => {
+  const productPosition = Products.findIndex((product) => id === product.id);
+
+  if (productPosition === -1) return null;
+
+  const oldProduct = Products[productPosition];
+  const newProduct = { ...oldProduct, title, price, description, count };
+
+  Users.splice(productPosition, 1, newProduct);
+};
+
+export {
+  Products,
+  getAll,
+  getProductById,
+  createProduct,
+  deleteById,
+  updateById,
+};
