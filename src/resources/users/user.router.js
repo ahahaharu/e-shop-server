e303bf3f2d281422c6caf6318f3afc112c2babe5
ahahaharu <andreybogdanovich06@gmail.com>
2023-04-12 22:39:46 +0300

    feat: user router done

--- src/resources/users/user.router.js
+++ src/resources/users/user.router.js
@@ -1,14 +1,84 @@
 import { Router } from 'express';
-
 import User from './user.model.js';
 import * as usersService from './user.service.js';
+import catchErrors from '../../common/catchErrors.js';
+import { StatusCodes } from "http-status-codes"
 
 const router = Router();
 
-router.route('/').get(async (req, res) => {
-  const users = await usersService.getAll();
-  // map user fields to exclude secret fields like "password"
-  res.json(users.map(User.toResponse));
-});
+router.route('/').get(
+  catchErrors(async (req, res) => {
+    const users = await usersService.getAll();
+
+    res.json(users.map(User.toResponse));
+  })
+);
+
+router.route('/').post(
+  catchErrors(async (req, res) => {
+    const { name, login, password } = req.body;
+
+    const user = await usersService.createUser({ name, login, password });
+
+    if (user) {
+      res.status(StatusCodes.CREATED).json(User.toResponse(user));
+    } else {
+      res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ code: 'USER_NOT_CREATE', msg: 'User not create' });
+    }
+  })
+);
+
+router.route('/:id').get(
+  catchErrors(async (req, res) => {
+    const { id } = req.params;
+
+    const user = await usersService.getById(id);
+
+    if (user) {
+      res.json(User.toResponse(user));
+    } else {
+      res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ code: 'USER_NOT_FOUND', msg: 'User not found' });
+    }
+  })
+);
+
+router.route('/:id').put(
+  catchErrors(async (req, res) => {
+    const { id } = req.params;
+    const { name, login, password } = req.body;
+
+    const user = await usersService.updateById({ id, name, login, password });
+
+    if (user) {
+      res.status(StatusCodes.OK).json(User.toResponse(user));
+    } else {
+      res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ code: 'USER_NOT_FOUND', msg: 'User not found' });
+    }
+  })
+);
+
+router.route('/:id').delete(
+  catchErrors(async (req, res) => {
+    const { id } = req.params;
+
+    const user = await usersService.deleteById(id);
+
+    if (!user) {
+      return res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ code: 'USER_NOT_FOUND', msg: 'User not found' });
+    }
+
+    return res
+      .status(StatusCodes.NO_CONTENT)
+      .json({ code: 'USER_DELETED', msg: 'The user has been deleted' });
+  })
+);
 
 export default router;
