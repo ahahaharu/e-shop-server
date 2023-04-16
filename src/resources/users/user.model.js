7dd4a2014a0da2bf07698d7fe90c09b6dc0e4752
ahahaharu <andreybogdanovich06@gmail.com>
2023-04-16 19:31:07 +0300

    refactor: fixed semicolon error

--- src/resources/users/user.model.js
+++ src/resources/users/user.model.js
@@ -1,7 +1,7 @@
 import { v4 as uuidv4 } from 'uuid';
 
 class User {
-  constructor({ id = uuidv4(), firstName = 'USER', lastName = 'LASTNAME' login = 'user', password = 'P@55w0rd', phoneNumber = '80294547812' } = {}) {
+  constructor({ id = uuidv4(), firstName = 'USER', lastName = 'LASTNAME', login = 'user', password = 'P@55w0rd', phoneNumber = '80294547812' } = {}) {
     this.id = id;
     this.firstName = firstName;
     this.lastName = lastName;
